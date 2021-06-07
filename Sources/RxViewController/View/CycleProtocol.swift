7c9aeb4f2eca74eb5c4c2364836522547a85b017
BugkingK <myway0710@naver.com>
2021-06-07 12:58:11 +0900

    🐛 nib bug fix

--- Sources/RxViewController/View/CycleProtocol.swift
+++ Sources/RxViewController/View/CycleProtocol.swift
@@ -57,19 +57,22 @@ public extension CycleProtocol {
 
             if let viewModel = viewModel as? Self.ViewModel {
                 viewController.viewModel = viewModel
+            } else {
+                Log.warning(d: "\(identifier)) Failed to initialize ViewModel.")
             }
 
             return viewController
         case .nib(let nibName):
             let nibName: String = nibName ?? .init(describing: self)
-            let nib: UINib = .init(nibName: nibName, bundle: viewModel.kind.bundle)
-
-            if let view = nib.instantiate(withOwner: nil, options: nil)[0] as? Self {
+            let bundle: Bundle = viewModel.kind.bundle ?? .main
+            if let viewController = bundle.loadNibNamed(nibName, owner: self, options: nil)?[0] as? Self {
                 if let viewModel = viewModel as? Self.ViewModel {
-                    view.bind(viewModel)
+                    viewController.bind(viewModel)
+                } else {
+                    Log.warning(d: "\(nibName)) Failed to initialize ViewModel.")
                 }
 
-                return view
+                return viewController
             } else {
                 fatalError("Please check the init value of ViewModel again.")
             }
