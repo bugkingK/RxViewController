4f5505615c296f2d3c9d9a62c26ba512b1c1f86c
bugkingK <myway0710@naver.com>
2021-06-23 15:16:42 +0900

    ✨ Add RxWindowController

---
+++ Sources/RxViewController/View/RxWindowController.swift
@@ -0,0 +1,57 @@
+//
+//  RxWindowController.swift
+//  
+//
+//  Created by Kimun Kwon on 2021/06/23.
+//
+
+#if os(macOS)
+import AppKit
+import RxSwift
+
+class RxWindowController<ViewModel: RxViewModel>: NSWindowController, CycleProtocol {
+    public var disposeBag: DisposeBag = .init()
+    public var viewModel: ViewModel!
+
+    public convenience required init(viewModel: ViewModel) {
+        switch viewModel.kind.type {
+        case .xib(let nibName):
+            let nibName: String = nibName ?? ""
+            self.init(windowNibName: nibName)
+            self.viewModel = viewModel
+        case .code:
+            self.init()
+            self.viewModel = viewModel
+        default:
+            fatalError("If an error occurs here, please register it as an issue.")
+        }
+    }
+    
+    public override init(window: NSWindow?) {
+        super.init(window: window)
+        self.initialize()
+    }
+
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+    
+    override func loadWindow() {
+        super.loadWindow()
+        layout()
+        bind(viewModel)
+    }
+
+    deinit {
+        Log.print(d: "DEINIT \(type(of: self))")
+    }
+
+    open func layout() {}
+
+    open func initialize() {}
+
+    open func bind(_ viewModel: ViewModel) {}
+}
+
+#endif
